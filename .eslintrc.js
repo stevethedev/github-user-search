ac654eb6b46095fb99619495aee0339df8e40d85
Steven Jimenez <stevenmjimenez@gmail.com>
2021-04-09 00:37:25 -0400

    Added intended dependencies and configurations

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  ignorePatterns: ['webpack.config.js'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'off',
+    'no-void': 'off',
+    'import/prefer-default-export': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/jsx-filename-extension': ['error', {extensions: ['.jsx', '.tsx']}],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        'module.css': 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx', 'module.css'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', 'module.css'],
+      },
+    },
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    devtool: !isProduction && 'cheap-module-source-map',
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/client.ts',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'assets/js/client.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: {loader: 'ts-loader'},
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {loader: 'style-loader'},
+            {
+              loader: "css-loader",
+              options: {modules: true}
+            },
+            {loader: '@teamsupercell/typings-for-css-modules-loader'},
+          ]
+        }
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.css'],
+    },
+  };
+};
